673a04b0a406e7f19cdcc0597b622519df6dd915
Aeri <victoriacarlangelo@gmail.com>
2025-03-06 22:19:38 +0800

    fifth commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-DISPLAY_WIDTH = 550.36
-DISPLAY_HEIGHT = 700.56
+DISPLAY_WIDTH = 432
+DISPLAY_HEIGHT = 672
 FPS = 60
 BACKGROUND_COLOR = (1, 1, 1, 1)  # White background
-GRID_SIZE = (168, 108)  # 198 rows, 150 columns
+GRID_SIZE = (164, 108)  # 198 rows, 150 columns
 
